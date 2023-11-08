b3fdbc6b06ef07a55766c8bafa200ae8e79f6d23
Tony C <105914325+TotoWolfy@users.noreply.github.com>
2023-11-08 08:56:42 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 //exercice 0:
 console.log("message javascript du serveur");
+var myApp = require("simple-hello-world-example");
+
+var msg = myApp.printMsg();
+
+const express = require("express");
+const app = express();
+app.use("/dream", express.static("public"));
+app.listen(3000);
